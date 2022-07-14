e69e77f55621e2c9ea0c3c77d2db8814ab242ea2
Willie Roberts <diagoza@me.com>
2022-07-14 14:05:04 +0200

    Updated acme install

--- acme-install.sh
+++ acme-install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-if [ -d "$HOME/acme.sh" ]; then
+if [ -d "$HOME/.acme.sh" ]; then
   exit
 fi
 
 mkdir -p /tmp/acme.sh
 mkdir -p "$HOME/.acme.sh"
-git clone -qq https://github.com/acmesh-official/acme.sh.git /tmp/acme.sh
+git clone https://github.com/acmesh-official/acme.sh.git /tmp/acme.sh
 pushd /tmp/acme.sh >/dev/null || return
 "/tmp/acme.sh/acme.sh" --install \
   --home "$HOME/.acme.sh" \
