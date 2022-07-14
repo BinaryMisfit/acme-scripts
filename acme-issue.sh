6d5fed038b068057ae6d7998c62795bf258a8c4b
Willie Roberts <diagoza@me.com>
2022-07-14 16:45:35 +0200

    Added dnssleep

--- acme-issue.sh
+++ acme-issue.sh
@@ -9,14 +9,12 @@ printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Checking %d domains\n' -1 ${#DOMAINS[@]}
 for ENTRY in "${DOMAINS[@]}"; do
   mapfile -td\, DOMAIN_INFO < <(printf "%s" "$ENTRY")
   printf -v DOMAIN_ARGS -- ' --domain %s' "${DOMAIN_INFO[@]%s'\n'}"
-  printf -v ACME_ISSUE '%s/.acme.sh/acme.sh --issue --dns dns_cloudns --ecc' "$HOME"
+  printf -v ACME_ISSUE '%s/.acme.sh/acme.sh --issue --dns dns_cloudns --dnssleep --ecc' "$HOME"
   printf -v ACME_ISSUE '%s --home \"%s/.acme.sh\" --config-home \"%s\"' "$ACME_ISSUE" "$HOME" "$LE_CONFIG_HOME"
   printf -v ACME_ISSUE '%s --log %s/.acme.sh/acme.sh.log' "$ACME_ISSUE" "$HOME"
   printf -v ACME_ISSUE '%s --cert-home \"%s\" %s%s' "$ACME_ISSUE" "$LE_CERT_HOME" "$ACME_SH_ARGS" "${DOMAIN_ARGS[@]}"
   DOMAIN_PATH="$LE_CERT_HOME/${DOMAIN_INFO[0]}"
   DOMAIN_CERT="$DOMAIN_PATH/${DOMAIN_INFO[0]}.cer"
-  printf "Domain Path: %s" "${DOMAIN_PATH}"
-  printf "Domain Cert: %s" "${DOMAIN_CERT}"
   if [ ! -f "$DOMAIN_CERT" ]; then
     eval "$ACME_ISSUE"
   fi
